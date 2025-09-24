107aff308afccfc1f04b6d854a5d95bb923e9b0c
Kaushani Hettiarachchige <kaushanihettiarachchige@gmail.com>
2025-09-24 19:10:53 +0530

    Fixed vulnerabilities - Missing Authentication and CSRF in ticket.routes.js file

--- backend/routes/ticket.routes.js
+++ backend/routes/ticket.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { verifyAuthentication } from "../middleware/auth.middleware.js";
 import TicketController from "../controllers/ticket.controller.js";
+import csrfProtection from "../middleware/csrf.middleware.js";
 
 const router = express.Router();
 
 router.route("/api/add-ticket").post(
     verifyAuthentication,
+    csrfProtection,
     TicketController.addTicket
 )
 
@@ -15,36 +17,40 @@ router.route("/api/reasons").get(
 )
 
 router.route("/api/tickets/:branchId").get(
-    // verifyAuthentication,
+    verifyAuthentication,
     TicketController.getAllTicketsByBranchId
 )
 
 // route to update a ticket by ticket id
 router.route("/api/update-ticket/:id").put(
     verifyAuthentication,
+    csrfProtection,
     TicketController.updateTicket
 )
 
 // update ticket reply
 router.route("/api/update-ticket-reply/:id").put(
-    // verifyAuthentication,
+    verifyAuthentication,
+    csrfProtection,
     TicketController.updateTicketReply
 )
 
 // update ticket status
 router.route("/api/update-ticket-status/:ticketId/:statusId").put(
     verifyAuthentication,
+    csrfProtection,
     TicketController.ticketStatusUpdate
 )
 
 // delete ticket
 router.route("/api/delete-ticket/:id").delete(
     verifyAuthentication,
+    csrfProtection,
     TicketController.deleteTicket
 )
 
 router.route("/api/tickets-by-nic/:nic").get(
-    // verifyAuthentication,
+    verifyAuthentication,
     TicketController.getTicketsByNic
 )
 
