06bca9d442d4d15b1264c0a8b453c537e0ba6f1c
Kaushani Hettiarachchige <kaushanihettiarachchige@gmail.com>
2025-09-24 18:56:10 +0530

    Fixed vulnerabilities - Missing Authentication in delivery.routes.js file

--- backend/routes/delivery.routes.js
+++ backend/routes/delivery.routes.js
@@ -1,25 +1,26 @@
 import express from "express";
 import { verifyAuthentication } from "../middleware/auth.middleware.js";
-import DeliveryPersonController from "../controllers/delivery.controller.js"
+import DeliveryPersonController from "../controllers/delivery.controller.js";
+import csrfProtection from "../middleware/csrf.middleware.js";
 
 const router = express.Router();
 
 // Add a new delivery person
-router.post("/api/add-delivery-person", verifyAuthentication, DeliveryPersonController.addDeliveryPerson);
+router.post("/api/add-delivery-person", verifyAuthentication, csrfProtection, DeliveryPersonController.addDeliveryPerson);
 
 // Get all delivery persons
 router.get("/api/get-all-delivery-persons", verifyAuthentication, DeliveryPersonController.getAllDeliveryPersons);
 
 // Update a delivery person by NIC
-router.put("/api/update-delivery-person/:nic", verifyAuthentication, DeliveryPersonController.updateDeliveryPerson);
+router.put("/api/update-delivery-person/:nic", verifyAuthentication, csrfProtection, DeliveryPersonController.updateDeliveryPerson);
 
 // Delete a delivery person by NIC
-router.delete("/api/delete-delivery-person/:nic", verifyAuthentication, DeliveryPersonController.deleteDeliveryPerson);
+router.delete("/api/delete-delivery-person/:nic", verifyAuthentication, csrfProtection, DeliveryPersonController.deleteDeliveryPerson);
 
 // Get all delivery persons NICs
-router.get("/api/get-all-delivery-persons-nics/:branchId", DeliveryPersonController.getAllDeliveryPersonsNics);
+router.get("/api/get-all-delivery-persons-nics/:branchId", verifyAuthentication, DeliveryPersonController.getAllDeliveryPersonsNics);
 
 // Get orders by delivery person NIC
-router.get("/api/get-orders-by-delivery-person-nic/:nic", DeliveryPersonController.getOrdersByDeliveryPersonNic);
+router.get("/api/get-orders-by-delivery-person-nic/:nic", verifyAuthentication, DeliveryPersonController.getOrdersByDeliveryPersonNic);
 
 export { router };
