562ce93ba6a0387b2d48e5854c3d32ca8ec0c5d7
Kaushani Hettiarachchige <kaushanihettiarachchige@gmail.com>
2025-09-24 17:52:49 +0530

    Fixed vulnerabilities - CSRF in order.routes.js file

--- backend/routes/order.routes.js
+++ backend/routes/order.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import OrderController from "../controllers/order.controller.js"; //Always add .js at the end
 import { verifyAuthentication } from "../middleware/auth.middleware.js";
+import csrfProtection from "../middleware/csrf.middleware.js"; //CSRF protection
 
 const router = express.Router();
 
 router.route("/api/add-order").post(
     verifyAuthentication,
+    csrfProtection,
     OrderController.addOrder
 );
 
@@ -21,11 +23,13 @@ router.route("/api/orderstatus").get(
 
 router.route("/api/orders-by-branch").post(
     verifyAuthentication,
+    csrfProtection,
     OrderController.getAllOrderByBranchId
 );
 
 router.route("/api/courier-fee").post(
     verifyAuthentication,
+    csrfProtection,
     OrderController.getCourierFee
 );
 
@@ -36,52 +40,56 @@ router.route("/api/order/:orderId").get(
 
 router.route("/api/update-order/:orderId").put(
     verifyAuthentication,
+    csrfProtecction,
     OrderController.updateOrder
 );
 
 router.route("/api/delete-order/:orderId").delete(
-    // verifyAuthentication,
+    verifyAuthentication,
+    csrfProtection,
     OrderController.deleteOrder
 );
 
 router.route("/api/receivedOrders/:branchId").get(
-    // verifyAuthentication,
+    verifyAuthentication,
     OrderController.getAllReceivedOrdersByBranchId
 );
 
 router.route("/api/assign-delivery-person/:orderId/:nic").put(
-    // verifyAuthentication,
+    verifyAuthentication,
+    csrfProtection,
     OrderController.assignDeliveryPerson
 );
 
 router.route("/api/incoming-orders/:branchId").get(
-    // verifyAuthentication,
+    verifyAuthentication,
     OrderController.getAllIncomingOrdersByBranchId
 );
 
 router.route("/api/update-order-status/:orderId/:status").put(
-    // verifyAuthentication,
+    verifyAuthentication,
+    csrfProtection,
     OrderController.updateOrderStatus
 );
 
 router.route("/api/received-orders-by-status/:branchId/:status").get(
-    // verifyAuthentication,
+    verifyAuthentication,
     OrderController.getReceivedOrdersByStatus
 );
 
 router.route("/api/order-view/:orderId").get(
-    // verifyAuthentication,
+    verifyAuthentication,
     OrderController.getOrderViewDetailsByOrderId
 );
 
 router.route("/api/check-order-existing-status/:orderId").get(
-    // verifyAuthentication,
+    verifyAuthentication,
     OrderController.checkOrderExistingStatus
 );
 
 // getAllAvailableOrdersByUserNic
 router.route("/api/orders-by-nic/:nic").get(
-    // verifyAuthentication,
+    verifyAuthentication,
     OrderController.getAllAvailableOrdersByUserNic
 );
 
