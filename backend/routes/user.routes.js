e7e5b5b4e38f169e3dc8d55a1bcb625aae48b222
Kaushani Hettiarachchige <kaushanihettiarachchige@gmail.com>
2025-09-24 19:23:22 +0530

    Fixed vulnerabilities - Missing Authentication and CSRF in user.routes.js file

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -7,14 +7,15 @@ import UserController from "../controllers/user.controller.js";
 
 //using auth middleware for logging out
 import { verifyAuthentication } from "../middleware/auth.middleware.js";
+import csrfProtection from "../middleware/csrf.middleware.js";
 
 const router = express.Router();
 
-router.route("/api/user/login").post(UserController.verifyCredentials);
+router.route("/api/user/login").post(csrfProtection, UserController.verifyCredentials);
 
 router
   .route("api/user/logout")
   //before logging out, checking auth status 
-  .post(verifyAuthentication, UserController.logOutUser);
+  .post(verifyAuthentication, csrfProtection, UserController.logOutUser);
 
 export { router };
