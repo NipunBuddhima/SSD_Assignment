c519066cc98439a77c1a430eddf58dc219bdb7ab
Kaushani Hettiarachchige <kaushanihettiarachchige@gmail.com>
2025-09-24 18:16:50 +0530

    Fixed vulnerabilities - CSRF in client.routes.js file

--- backend/routes/client.routes.js
+++ backend/routes/client.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import ClientController from "../controllers/client.controller.js"; //Always add .js at the end
 import { verifyAuthentication } from "../middleware/auth.middleware.js";
+import csrfProtection from "../middleware/csrf.middleware.js" //CSRF Protection
 
 const router = express.Router();
 
 router.route("/api/add-client").post(
     verifyAuthentication,
+    csrfProtection,
     ClientController.addClient
 )
 
@@ -21,21 +23,24 @@ router.route("/api/clients/nic").get(
 
 router.route("/api/update-client/:id").put(
     verifyAuthentication,
+    csrfProtection, 
     ClientController.updateClient
 );
 
 router.route("/api/client/:id").delete(
     verifyAuthentication,
+    csrfProtection,
     ClientController.deleteClient
 );
 
 router.route("/api/client-details/:id").get(
-    // verifyAuthentication,
+    verifyAuthentication,
     ClientController.getClientDetails
 );
 
 router.route("/api/update-client-password/:id").put(
     verifyAuthentication,
+    csrfProtection,
     ClientController.updateClientPassword
 );
 
