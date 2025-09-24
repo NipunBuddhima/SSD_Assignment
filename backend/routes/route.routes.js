5b4f30c9ff3566631de84a2ec59d9fc2527449a1
Kaushani Hettiarachchige <kaushanihettiarachchige@gmail.com>
2025-09-24 19:05:22 +0530

    Fixed vulnerabilities - Missing Authentication and CSRF in route.routes.js file

--- backend/routes/route.routes.js
+++ backend/routes/route.routes.js
@@ -1,40 +1,44 @@
 import express from "express";
 import { verifyAuthentication } from "../middleware/auth.middleware.js";
 import RouteController from "../controllers/route.controller.js";
+import csrfProtection from "../middleware/csrf.middleware.js";
 
 const router = express.Router();
 
 router.route("/api/add-route").post(
     verifyAuthentication,
+    csrfProtection,
     RouteController.addRoute
 )
 
 router.route("/api/routes").get(
-    // verifyAuthentication,
+    verifyAuthentication,
     RouteController.getAllRoutes
 )
 
 // get all routes for table
 router.route("/api/routes-for-table").get(
-    // verifyAuthentication,
+    verifyAuthentication,
     RouteController.getAllRoutesForTable
 )
 
 // update route
 router.route("/api/update-route").put(
     verifyAuthentication,
+    csrfProtection,
     RouteController.updateRoute
 )
 
 // delete route
 router.route("/api/delete-route/:routeId").delete(
     verifyAuthentication,
+    csrfProtection,
     RouteController.deleteRoute
 )
 
 
 router.route("/api/branches-by-transport-agent-nic/:nic").get(
-    // verifyAuthentication,
+    verifyAuthentication,
     RouteController.getBranchesByTransportAgentNic
 )
 
