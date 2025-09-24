7b009866774830b32a7418b0379f1eaff2e71f1b
Kaushani Hettiarachchige <kaushanihettiarachchige@gmail.com>
2025-09-24 18:49:28 +0530

    Fixed vulnerabilities - Missing Authentication in dashboard.routes.js file

--- backend/routes/dashboard.routes.js
+++ backend/routes/dashboard.routes.js
@@ -1,26 +1,26 @@
 import express from "express";
 import DashboardController from "../controllers/dashboard.controller";
-import { verifyAuthentication } from "../middleware/auth.middleware";
+import { verifyAuthentication } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.route("/api/getTotalOrderCountOfBranch").get(
-    // verfifyAuthentication
+    verifyAuthentication,
     DashboardController.getTotalOrderCountOfBranch
 )
 
 router.route("/api/getTotalReceivedOrderCountOfBranch").get(
-    // VerifyAuthentication
+    verifyAuthentication,
     DashboardController.getTotalReceivedOrderCount
 )
 
 router.route("/api/getTotalOpenedTicketCountOfBranch").get(
-    // VerifyAuthentication
+    verifyAuthentication,
     DashboardController.getTotalOpenedTicketCount
 )
 
 router.route("/api/getTotalFeedbackCountOfBranch").get(
-    // VerfityAuthentication
+    verfityAuthentication,
     DashboardController.getTotalFeedbackCount
 )
 
