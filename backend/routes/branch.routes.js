ea1f42c86ea43b95f1e2c1b18745cd4c6b45e272
Kaushani Hettiarachchige <kaushanihettiarachchige@gmail.com>
2025-09-24 18:44:18 +0530

    Fixed vulnerabilities - CSRF in branch.routes.js file

--- backend/routes/branch.routes.js
+++ backend/routes/branch.routes.js
@@ -1,33 +1,38 @@
 import express from "express";
 import BranchController from "../controllers/branch.controller.js"; //Always add .js at the end
 import { verifyAuthentication } from "../middleware/auth.middleware.js";
+import csrfProtection from "../middleware/csrf.middleware.js";
 
 const router = express.Router();
 
 router.route("/api/add-branch").post(
   verifyAuthentication,
+  csrfProtection,
   BranchController.addBranch
 );
 
 router.route("/api/branch-id-by-nic").post(
   verifyAuthentication,
+  csrfProtection,
   BranchController.getBranchIdByBranchManagerNIC
 );
 
 router.route("/api/branches").get(
-  // verifyAuthentication,
+  verifyAuthentication,
   BranchController.getAllBranches
 );
 
 // update branch
 router.route("/api/update-branch/:branchId").put(
   verifyAuthentication,
+  csrfProtection,
   BranchController.updateBranch
 );
 
 // delete branch
 router.route("/api/delete-branch/:branchId").delete(
   verifyAuthentication,
+  csrfProtection,
   BranchController.deleteBranch
 );
 
